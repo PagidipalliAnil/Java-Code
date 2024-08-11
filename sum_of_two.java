cd660769a4d5dc7fa124e72e19772520a7c9daa8
PagidipalliAnil <pagidipallianil3001@gmail.com>
2024-08-11 15:05:38 +0530

    Add files via upload

---
+++ sum_of_two.java
@@ -0,0 +1,10 @@
+public class sum_of_two {
+    public static void main(String[] args){
+        int x = 5;
+        int y = 6;
+        int sum;
+        sum = x + y;
+        System.out.println("Sum is:" + sum);
+    }
+
+}
